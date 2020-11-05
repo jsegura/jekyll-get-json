45352783e9a1edfdebc506cee623864de43514f6
Markus <mdoering@gbif.org>
2020-11-05 13:24:24 +0100

    Use ssl and basic auth

--- lib/jekyll-get-json/converter.rb
+++ lib/jekyll-get-json/converter.rb
@@ -1,8 +1,9 @@
 require "jekyll"
+require 'uri'
 require 'net/http'
+require 'net/https'
 require 'json'
 require 'deep_merge'
-require 'open-uri'
 
 
 module JekyllGetJson
@@ -11,7 +12,6 @@ module JekyllGetJson
     priority :highest
 
     def generate(site)
-
       config = site.config['jekyll_get_json']
       if !config
         warn "No config".yellow
@@ -24,21 +24,31 @@ module JekyllGetJson
       config.each do |d|
         begin
           uri = URI(d['json'])
-          req = Net::HTTP::Get.new(uri.request_uri)
           user = d['user']
           pass = d['pass']
-          if user
-            req.basic_auth user, pass
-          end
-          resp = Net::HTTP.start(uri.hostname, uri.port) {|http|
-            http.request(req)
-          }
-          source = JSON.parse(resp)
-          target = site.data[d['data']]
-          if target
-            target.deep_merge(source)
-          else
-            site.data[d['data']] = source
+
+          puts "Reading JSON from #{uri}"
+          Net::HTTP.start(uri.host, uri.port,
+            :use_ssl => uri.scheme == 'https', 
+            :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+
+            req = Net::HTTP::Get.new uri.request_uri
+            if user
+              req.basic_auth user, pass
+            end
+            resp = http.request req # Net::HTTPResponse object
+
+            if resp.code != "200"
+              warn "Bad JSON response #{resp.code}: #{resp.message}"
+              next
+            end
+            source = JSON.parse(resp.body)
+            target = site.data[d['data']]
+            if target
+              target.deep_merge(source)
+            else
+              site.data[d['data']] = source
+            end
           end
         rescue
           next
